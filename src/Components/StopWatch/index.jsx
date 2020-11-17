94d4ff299522ec02b3c0a533e07554275ec22283
danwhat <daanwhat@gmail.com>
2020-11-17 13:20:16 -0300

    comitão bonitão

--- src/Components/StopWatch/index.jsx
+++ src/Components/StopWatch/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import Screen from '../../Components/Screen'
+import './style.css'
 
 const Stopwatch = () => {
 
@@ -9,16 +11,16 @@ const Stopwatch = () => {
   const [status, setStatus] = useState(false);
 
   useEffect(() => {
-    if(status) {
-      setTimeout(() => setCount(count+10), 10)
+    if (status) {
+      setTimeout(() => setCount(count + 10), 10)
     }
   }, [status, count])
 
   const convertTime = (count) => {
 
-    const constConvert = {hour: 3600000, minute: 60000, second: 1000}
+    const constConvert = { hour: 3600000, minute: 60000, second: 1000 }
 
-    const timeCon = {hour: 0, minute: 0, second: 0, milisecond: 0}
+    const timeCon = { hour: 0, minute: 0, second: 0, milisecond: 0 }
 
     timeCon.hour = Math.floor(count / constConvert.hour);
     timeCon.minute = Math.floor((count - timeCon.hour * constConvert.hour) / constConvert.minute);
@@ -26,18 +28,36 @@ const Stopwatch = () => {
     timeCon.milisecond = Math.floor(count - timeCon.hour * constConvert.hour - timeCon.minute * constConvert.minute - timeCon.second * constConvert.second);
 
     return timeCon;
-  } 
-  
+  }
+
   return (
-    <div>
-      <h1>{convertTime(count).hour}</h1>
-      <h1>{convertTime(count).minute}</h1>
-      <h1>{convertTime(count).second}</h1>
-      <p>{convertTime(count).milisecond}</p>
-      <button onClick={() => setStatus(true)}>Iniciar</button>
-      <button onClick={() => setStatus(false)}>Parar</button>
+    <Screen>
+      <div className="SelectStopWatch">
+        <div className="timer">
+          <div>
+            {convertTime(count).hour > 0 && <h1>{convertTime(count).hour}:</h1>}
+            <h1>{convertTime(count).minute < 10
+              ? '0' + convertTime(count).minute + ":"
+              : convertTime(count).minute + ":"}</h1>
+
+            <h1>{convertTime(count).second < 10
+              ? '0' + convertTime(count).second
+              : convertTime(count).second}</h1>
+          </div>
+          <p>{convertTime(count).milisecond === 0
+            ? '000'
+            : convertTime(count).milisecond < 100
+              ? '0' + convertTime(count).milisecond
+              : convertTime(count).milisecond}
+          </p>
+        </div>
+        <div>
+          <button onClick={() => setStatus(true)}>Iniciar</button>
+          <button onClick={() => setStatus(false)}>Parar</button>
+        </div>
+      </div>
       <Link to="/">Voltar</Link>
-    </div>
+    </Screen>
   )
 }
 
